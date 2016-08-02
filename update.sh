b7e21e766c3967e4a3f31c013dd7d557f74aa828
labrick <yananback@gmail.com>
2016-08-02 10:26:44 +0800

    增加更新代码脚本--personal

---
+++ update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "start update android ..."
+cd android/system
+if [ ! -d ".tmp" ]; then
+    mkdir .tmp
+fi
+if [ -d ".tmp/core" ]; then
+    rm -rf .tmp/core
+fi
+mv -f core .tmp/core
+cp -r /home/lab302/robot/R16-Parrot_SDK/android/system/core/ ./
+cd ../../
+echo "update android ok"
+
+echo "start update linux ..."
+sleep 1
+cd lichee/linux-3.4/arch/arm
+if [ ! -d ".tmp" ]; then
+    mkdir .tmp
+fi
+if [ -d ".tmp/mach-sunxi" ]; then
+    rm -rf .tmp/mach-sunxi
+fi
+mv -f mach-sunxi .tmp/mach-sunxi
+cp -r /home/lab302/robot/R16-Parrot_SDK/lichee/linux-3.4/arch/arm/mach-sunxi/ ./
+cd ../../../../
+echo "update android ok"
+echo
+pwd
+echo
+echo "---------------------check code changed-----------------"
+git status
