70282c7ede97a6d216e3625c38eb7f13e20f172f
labrick <yananback@gmail.com>
2016-08-17 23:52:20 +0800

    采用宏解决目录问题

--- update.sh
+++ update.sh
@@ -3,32 +3,50 @@
 LINUXDIR=/mnt/hdd2/ProjectGroup/R16-Parrot_SDK
 ANDROIDDIR=/home/lab302/robot/R16-Parrot_SDK
 
-echo "start update android ..."
-cd android/system
-if [ ! -d ".tmp" ]; then
-    mkdir .tmp
-fi
-if [ -d ".tmp/core" ]; then
-    rm -rf .tmp/core
-fi
-mv -f core .tmp/core
-cp -r $ANDROIDDIR/android/system/core/ ./
-cd ../../
-echo "update android ok"
+#-------------------------------------------------
+ANDROIDFILE=core
+ANDROIDSUBDIR=android/system
+mkdir -p .tmp/$ANDROIDSUBDIR
 
-echo "start update linux ..."
+echo "start update $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE ..." 
 sleep 1
-cd lichee/linux-3.4/arch/
-if [ ! -d ".tmp" ]; then
-    mkdir .tmp
+if [ -d ".tmp/$ANDROIDSUBDIR/$ANDROIDFILE" ]; then
+    rm -rf .tmp/$ANDROIDSUBDIR/$ANDROIDFILE
 fi
-if [ -d ".tmp/arm" ]; then
-    rm -rf .tmp/arm
+mv -f $ANDROIDSUBDIR/$ANDROIDFILE .tmp/$ANDROIDSUBDIR
+cp -r $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE $ANDROIDSUBDIR
+echo "update $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE ok"
+
+#-------------------------------------------------
+LINUXFILE=arm
+LINUXSUBDIR=lichee/linux-3.4/arch
+mkdir -p .tmp/$LINUXSUBDIR
+
+echo "start update $LINUXDIR/$LINUXSUBDIR/$LINUXFILE ..."
+sleep 1
+if [ -d ".tmp/$LINUXSUBDIR/$LINUXFILE" ]; then
+    rm -rf .tmp/$LINUXSUBDIR/$LINUXFILE
 fi
-mv -f arm .tmp/arm
-cp -r $LINUXDIR/lichee/linux-3.4/arch/arm/ ./
-cd ../../../
-echo "update android ok"
+mv -f $LINUXSUBDIR/$LINUXFILE .tmp/$LINUXSUBDIR
+cp -r $LINUXDIR/$LINUXSUBDIR/$LINUXFILE $LINUXSUBDIR 
+echo "update $LINUXDIR/$LINUXSUBDIR/$LINUXFILE ok"
+
+#-------------------------------------------------
+PCTOOLFILE=pack
+PCTOOLSUBDIR=lichee/tools
+mkdir -p .tmp/$PCTOOLSUBDIR
+
+echo "start update $LINUXDIR/$PCTOOLSUBDIR/$PCTOOLFILE ..."
+sleep 1
+if [ -d ".tmp/$PCTOOLSUBDIR/$PCTOOLFILE" ]; then
+    rm -rf .tmp/$PCTOOLSUBDIR/$PCTOOLFILE
+fi
+mv -f $PCTOOLSUBDIR/$PCTOOLFILE .tmp/$PCTOOLSUBDIR
+cp -r $LINUXDIR/$PCTOOLSUBDIR/$PCTOOLFILE $PCTOOLSUBDIR
+echo "update $LINUXDIR/$PCTOOLSUBDIR/$PCTOOLFILE ok"
+
+#-------------------------------------------------
+
 echo
 echo "---------------------check code changed-----------------"
 git status
