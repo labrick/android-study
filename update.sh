65ca9ccba8bffc2fb1e4ed2f25f2312a69bbfe07
labrick <yananback@gmail.com>
2016-08-25 16:02:14 +0800

    将要更新的目录整合到一个集合中，方便管理，采用for循环遍历¼w:w

--- update.sh
+++ update.sh
@@ -1,70 +1,63 @@
 #!/bin/bash
 
+# 注意字符串截取采用的是SDK/作为分隔符
+
 LINUXDIR=/mnt/hdd2/ProjectGroup/R16-Parrot_SDK
 ANDROIDDIR=/home/lab302/robot/R16-Parrot_SDK
 
-#-------------------------------------------------
-ANDROIDFILE=core
-ANDROIDSUBDIR=android/system
-rm -rf .tmp/$ANDROIDSUBDIR
-mkdir -p .tmp/$ANDROIDSUBDIR $ANDROIDSUBDIR
+update_dir_list=(
+${ANDROIDDIR}/android/system/core
+${ANDROIDDIR}/android/frameworks/base/core
+${LINUXDIR}/lichee/linux-3.4/arch/arm
+${LINUXDIR}/lichee/tools/pack
+)
 
-echo "start update $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE ..." 
-sleep 1
-if [ -d ".tmp/$ANDROIDSUBDIR/$ANDROIDFILE" ]; then
-    rm -rf .tmp/$ANDROIDSUBDIR/$ANDROIDFILE
+function update_error()
+{
+    echo -e "\033[47;31mERROR: $*\033[0m"
+}
+
+function update_warn()
+{
+    echo -e "\033[47;34mWARN: $*\033[0m"
+}
+
+function update_info()
+{
+    echo -e "\033[47;30mINFO: $*\033[0m"
+}
+
+if [ $(basename `pwd`) != "android-study" ] ; then
+    echo "Please run at the top directory of anroid-study"
+    exit 1
 fi
-mv -f $ANDROIDSUBDIR/$ANDROIDFILE .tmp/$ANDROIDSUBDIR
-cp -r $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE $ANDROIDSUBDIR
-echo "update $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE ok"
 
-#-------------------------------------------------
-ANDROIDFILE=core
-ANDROIDSUBDIR=android/frameworks/base
-rm -rf .tmp/$ANDROIDSUBDIR
-mkdir -p .tmp/$ANDROIDSUBDIR $ANDROIDSUBDIR
+for update_dir in ${update_dir_list[@]}; do
+    BASENAME=$(basename ${update_dir})
+    # ${update_dir#*SDK/}   #号是运算符，*SDK/表示从左边开始删除第一个SDK/号及左边的所有字符
+    DIRNAME=$(dirname ${update_dir#*SDK/})
+    
+    echo ${update_dir}
+    echo ${DIRNAME}
+    echo ${BASENAME}
 
-echo "start update $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE ..." 
-sleep 1
-if [ -d ".tmp/$ANDROIDSUBDIR/$ANDROIDFILE" ]; then
-    rm -rf .tmp/$ANDROIDSUBDIR/$ANDROIDFILE
-fi
-mv -f $ANDROIDSUBDIR/$ANDROIDFILE .tmp/$ANDROIDSUBDIR
-cp -r $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE $ANDROIDSUBDIR
-echo "update $ANDROIDDIR/$ANDROIDSUBDIR/$ANDROIDFILE ok"
-
-
-#-------------------------------------------------
-LINUXFILE=arm
-LINUXSUBDIR=lichee/linux-3.4/arch
-rm -rf .tmp/$LINUXSUBDIR
-mkdir -p .tmp/$LINUXSUBDIR $LINUXSUBDIR
-
-echo "start update $LINUXDIR/$LINUXSUBDIR/$LINUXFILE ..."
-sleep 1
-if [ -d ".tmp/$LINUXSUBDIR/$LINUXFILE" ]; then
-    rm -rf .tmp/$LINUXSUBDIR/$LINUXFILE
-fi
-mv -f $LINUXSUBDIR/$LINUXFILE .tmp/$LINUXSUBDIR
-cp -r $LINUXDIR/$LINUXSUBDIR/$LINUXFILE $LINUXSUBDIR 
-echo "update $LINUXDIR/$LINUXSUBDIR/$LINUXFILE ok"
-
-#-------------------------------------------------
-PCTOOLFILE=pack
-PCTOOLSUBDIR=lichee/tools
-rm -rf .tmp/$PCTOOLSUBDIR
-mkdir -p .tmp/$PCTOOLSUBDIR $PCTOOLSUBDIR
-
-echo "start update $LINUXDIR/$PCTOOLSUBDIR/$PCTOOLFILE ..."
-sleep 1
-if [ -d ".tmp/$PCTOOLSUBDIR/$PCTOOLFILE" ]; then
-    rm -rf .tmp/$PCTOOLSUBDIR/$PCTOOLFILE
-fi
-mv -f $PCTOOLSUBDIR/$PCTOOLFILE .tmp/$PCTOOLSUBDIR
-cp -r $LINUXDIR/$PCTOOLSUBDIR/$PCTOOLFILE $PCTOOLSUBDIR
-echo "update $LINUXDIR/$PCTOOLSUBDIR/$PCTOOLFILE ok"
-
-#-------------------------------------------------
+    if [ ! -d ${update_dir} ]; then
+        update_error "${update_dir} doesn't exit!!"
+        exit 1
+    fi
+    
+    rm -rf .tmp/${DIRNAME}
+    mkdir -p .tmp/${DIRNAME} ${DIRNAME}
+    
+    echo "start update ${update_dir} ..." 
+    sleep 1
+    if [ -d ".tmp/${DIRNAME}/${BASENAME}" ]; then
+        rm -rf .tmp/${DIRNAME}/${BASENAME}
+    fi
+    mv -f ${DIRNAME}/${BASENAME} .tmp/${DIRNAME}
+    cp -r ${update_dir} ${DIRNAME}
+    echo "update ${update_dir} ok"
+done
 
 echo
 echo "---------------------check code changed-----------------"
