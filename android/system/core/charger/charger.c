0a33e7dc50d489dc9fdf005a9c31de2a720719f4
labrick <yananback@gmail.com>
2016-10-23 17:50:50 +0800

    分析充电时动画切换的实现，还没看uevent和graphic相关的

--- android/system/core/charger/charger.c
+++ android/system/core/charger/charger.c
@@ -88,7 +88,7 @@
 struct key_state {
     bool pending;
     bool down;
-    int64_t timestamp;
+    int64_t timestamp;      // key->down时的时间点
 };
 
 struct power_supply {
@@ -103,34 +103,34 @@ struct power_supply {
 };
 
 struct frame {
-    const char *name;
-    int disp_time;
-    int min_capacity;
+    const char *name;       // 每帧图片名称
+    int disp_time;          // 显示时间
+    int min_capacity;       // 该帧对应的容量值
     bool level_only;
 
     gr_surface surface;
 };
 
 struct animation {
-    bool run;
+    bool run;                       // 表示动画正在运行
 
-    struct frame *frames;
-    int cur_frame;
-    int num_frames;
+    struct frame *frames;           // 存放每帧的信息，数组名
+    int cur_frame;                  // 当前第几帧
+    int num_frames;                 // 总共多少帧
 
-    int cur_cycle;
-    int num_cycles;
+    int cur_cycle;                  // 当前周期
+    int num_cycles;                 // 总共周期数
 
     /* current capacity being animated */
-    int capacity;
+    int capacity;                   // 当前电池电量
 
-    bool freeze;
+    bool freeze;                    // 是否显示静态动画
 };
 
 struct charger {
-    int64_t next_screen_transition;
-    int64_t next_key_check;
-    int64_t next_pwr_check;
+    int64_t next_screen_transition; // 下帧动画开始时间(本次动画结束时间)
+    int64_t next_key_check;         // 下次按键检查的时间，如果不检查就置为-1
+    int64_t next_pwr_check;         // 下次检测DC的时间，如果不检查就置为-1
 
     struct key_state keys[KEY_MAX + 1];
     int uevent_fd;
@@ -139,7 +139,7 @@ struct charger {
     int num_supplies;
     int num_supplies_online;
 
-    struct animation *batt_anim;
+    struct animation *batt_anim;        // 存放动画信息
     gr_surface surf_unknown;
 
     struct power_supply *battery;
@@ -864,12 +864,15 @@ static void redraw_screen(struct charger *charger)
 {
     struct animation *batt_anim = charger->batt_anim;
 
+    // 清屏
     clear_screen();
 
     /* try to display *something* */
     if (batt_anim->capacity < 0 || batt_anim->num_frames == 0)
+        // 电量值未知状态或者就没有找到电量显示动画，直接显示个异类
         draw_unknown(charger);
     else
+        // 显示充电动画
         draw_battery(charger);
     gr_flip();
 }
@@ -919,6 +922,9 @@ static bool is_overtemp_animation_run(struct animation *anim)
                 return false;
 }
 
+// 作用：我们在状态的过程中,充电logo的电量是要增加的，比如电量
+// 是20%时，要从第一格开始闪烁；如果是80%时，则要从第三格开始闪烁，
+// 电量显示就是通过这个函数来计算实现的。
 static void update_screen_state(struct charger *charger, int64_t now)
 {
     struct animation *batt_anim = charger->batt_anim;
@@ -929,10 +935,11 @@ static void update_screen_state(struct charger *charger, int64_t now)
         return;
 
     /* animation is over, blank screen and leave */
+    // 当前周期==总的周期，说明动画结束，然后黑屏离开
     if (batt_anim->cur_cycle == batt_anim->num_cycles) {
-        reset_animation(batt_anim);
+        reset_animation(batt_anim);     // 动画初始化(周期/帧/运行状态/..)
         charger->next_screen_transition = -1;
-        gr_fb_blank(true);
+        gr_fb_blank(true);              // 黑屏，方便下次操作？
         LOGV("[%lld] animation done\n", now);
         if (charger->num_supplies_online > 0)
             request_suspend(true);
@@ -942,23 +949,26 @@ static void update_screen_state(struct charger *charger, int64_t now)
     disp_time = batt_anim->frames[batt_anim->cur_frame].disp_time;
 
     /* animation starting, set up the animation */
+    /* 启动动画 */
     if (batt_anim->cur_frame == 0) {
         int batt_cap;
         int ret;
 
         LOGV("[%lld] animation starting\n", now);
-        batt_cap = get_battery_capacity(charger);
+        batt_cap = get_battery_capacity(charger);       // 获得电池当前的电量
         if (batt_cap >= 0 && batt_anim->num_frames != 0) {
             int i;
 
             /* find first frame given current capacity */
+            /* 找到第一个大于当前电量值的帧图 */
             for (i = 1; i < batt_anim->num_frames; i++) {
                 if (batt_cap < batt_anim->frames[i].min_capacity)
                     break;
             }
-            batt_anim->cur_frame = i - 1;
+            batt_anim->cur_frame = i - 1;   // 这样就是第一个小于当前电量值的帧图
 
             /* show the first frame for twice as long */
+            /* 第一帧显示时间是其默认时间的二倍 */
             disp_time = batt_anim->frames[batt_anim->cur_frame].disp_time * 2;
         }
 
@@ -966,10 +976,12 @@ static void update_screen_state(struct charger *charger, int64_t now)
     }
 
     /* unblank the screen  on first cycle */
+    /* 第一周期不能黑屏了 */
     if (batt_anim->cur_cycle == 0)
         gr_fb_blank(false);
 
     /* draw the new frame (@ cur_frame) */
+    /* 绘制新的一帧图像 */
     redraw_screen(charger);
 
     /* if we don't have anim frames, we only have one image, so just bump
@@ -982,6 +994,8 @@ static void update_screen_state(struct charger *charger, int64_t now)
         return;
     }
 
+    // 如果上面执行通过就会直接退出，运行到这里说明电量状态及动画帧数没什么问题
+    // ，这里设置静态动画切换时间
     if(batt_anim->freeze == true){
         LOGV("display static battery capacity frame\n");
         charger->next_screen_transition = now + BATTERY_UNKNOWN_TIME;
@@ -989,20 +1003,26 @@ static void update_screen_state(struct charger *charger, int64_t now)
         return;
     }
 
+    // 运行到这里说明电池电量状态没问题/动画帧数没问题/设置也不是静态动画
     /* schedule next screen transition */
+    /* 动画帧之间的切换时间 */
     charger->next_screen_transition = now + disp_time;
 
     /* advance frame cntr to the next valid frame
      * if necessary, advance cycle cntr, and reset frame cntr
      */
+    /* 指向下一个有效帧 */
     batt_anim->cur_frame++;
 
     /* if the frame is used for level-only, that is only show it when it's
      * the current level, skip it during the animation.
      */
+    /* 如果当前帧仅仅在某个特定等级使用，那就在动画中跳过这一帧。 */
     while (batt_anim->cur_frame < batt_anim->num_frames &&
            batt_anim->frames[batt_anim->cur_frame].level_only)
         batt_anim->cur_frame++;
+
+    // 构成帧循环
     if (batt_anim->cur_frame >= batt_anim->num_frames) {
         batt_anim->cur_cycle++;
         batt_anim->cur_frame = 0;
@@ -1010,6 +1030,7 @@ static void update_screen_state(struct charger *charger, int64_t now)
         /* don't reset the cycle counter, since we use that as a signal
          * in a test above to check if animation is over
          */
+        /* 不要清零cycle计数器，因为我们会把这个值当作动画是否结束的信号 */
     }
 }
 
@@ -1068,29 +1089,31 @@ static void process_key(struct charger *charger, int code, int64_t now)
     struct key_state *key = &charger->keys[code];
     int64_t next_key_check;
 
-    if (code == KEY_POWER) {
+    if (code == KEY_POWER) {        // 电源键
         if (key->down) {
-            int64_t reboot_timeout = key->timestamp + POWER_ON_KEY_TIME;
-            if (now >= reboot_timeout) {
+            int64_t reboot_timeout = key->timestamp + POWER_ON_KEY_TIME;    // 2s
+            if (now >= reboot_timeout) {        // 如果长按power键，就重启，也就是开机
                 LOGI("[%lld] rebooting\n", now);
-                android_reboot(ANDROID_RB_RESTART, 0, 0);
+                android_reboot(ANDROID_RB_RESTART, 0, 0);   // 重启，也就是开机
             } else {
                 /* if the key is pressed but timeout hasn't expired,
                  * make sure we wake up at the right-ish time to check
                  */
+                // 设置下一次的按键检测时间为：按键按下的时间+开机长按时间(2s)
                 set_next_key_check(charger, key, POWER_ON_KEY_TIME);
             }
         } else {
             /* if the power key got released, force screen state cycle */
+            // 非显示电池损坏动画
             if (key->pending && !is_overtemp_animation_run(&battery_overtemp_animation)) {
-                if(!charger->batt_anim->run){
+                if(!charger->batt_anim->run){       // 当前没有显示充电动画，则显示
                         LOGV("[%lld] show animation!\n", now);
                         request_suspend(false);
                         reset_animation(charger->batt_anim);
                         kick_animation(charger->batt_anim);
                         set_backlight(197);
                 }
-                else{
+                else{   // 当前正在显示充电动画，则关闭动画
                         LOGV("[%lld] close animation!\n", now);
                         reset_animation(charger->batt_anim);
                         charger->next_screen_transition = -1;
@@ -1106,15 +1129,19 @@ static void process_key(struct charger *charger, int code, int64_t now)
 
 static void handle_input_state(struct charger *charger, int64_t now)
 {
+    // 调用按键处理函数
     process_key(charger, KEY_POWER, now);
 
+    // 下次检查按键的时间控制
     if (charger->next_key_check != -1 && now > charger->next_key_check)
+        // 检查完毕就置为-1，在wait_next_event中设置下次检查的时间
         charger->next_key_check = -1;
 }
 
 static void handle_power_supply_state(struct charger *charger, int64_t now)
 {
     if (charger->num_supplies_online == 0) {
+        // 如果没有在显示电池损坏动画
         if(!is_overtemp_animation_run(&battery_overtemp_animation)){
             request_suspend(false);
             set_backlight(197);
@@ -1123,10 +1150,11 @@ static void handle_power_supply_state(struct charger *charger, int64_t now)
         }
 
         if (charger->next_pwr_check == -1) {
+            // 下次DC检测的时间为不插入关机时间(3s)
             charger->next_pwr_check = now + UNPLUGGED_SHUTDOWN_TIME;
             LOGI("[%lld] device unplugged: shutting down in %lld (@ %lld)\n",
                  now, UNPLUGGED_SHUTDOWN_TIME, charger->next_pwr_check);
-        } else if (now >= charger->next_pwr_check) {
+        } else if (now >= charger->next_pwr_check) {    // 按键DC拔出，则关机
             LOGI("[%lld] shutting down\n", now);
             android_reboot(ANDROID_RB_POWEROFF, 0, 0);
         } else {
@@ -1244,22 +1272,27 @@ static int input_callback(int fd, short revents, void *data)
     return 0;
 }
 
+// 这个函数判断按键状态，DC是否插拔。
+// 如果长按开机：执行android_reboot(ANDROID_RB_RESTART,0, 0);
+// 如果拔出DC:执行android_reboot(ANDROID_RB_POWEROFF,0, 0);
 static void event_loop(struct charger *charger)
 {
     int ret;
 
     while (true) {
-        int64_t now = curr_time_ms();
+        // 这个时间来判断，有没有屏幕超时，如果超时关闭屏幕充电logo显示。
+        int64_t now = curr_time_ms();       // (1)、获得当前时间
 
         LOGV("[%lld] event_loop()\n", now);
         handle_battery_state(charger, now);
-        handle_input_state(charger, now);
-        handle_power_supply_state(charger, now);
+        handle_input_state(charger, now);   // (2)、检查按键状态
+        handle_power_supply_state(charger, now);    // (3)、检查DC是否拔出
         handle_leds_state(charger, now);
 
         /* do screen update last in case any of the above want to start
          * screen transitions (animations, etc)
          */
+        // (4)、对按键时间状态标志位的判断，显示不同电量的充电logo
         update_screen_state(charger, now);
 
         wait_next_event(charger, now);
@@ -1315,9 +1348,9 @@ int main(int argc, char **argv)
     LOGI("--------------- STARTING CHARGER MODE ---------------\n");
 
     gr_init();
-    gr_font_size(&char_width, &char_height);
+    gr_font_size(&char_width, &char_height);    // (1)、初始化graphics，包括buf大小
 
-    ev_init(input_callback, charger);
+    ev_init(input_callback, charger);           // (2)、初始化按键
 
     fd = uevent_open_socket(64*1024, true);
     if (fd >= 0) {
@@ -1325,6 +1358,11 @@ int main(int argc, char **argv)
         ev_add_fd(fd, uevent_callback, charger);
     }
     charger->uevent_fd = fd;
+    // (3)、创建/sys/class/power_supply结点，把socket信息通知应用层
+    // uevent_open_socket这个函数是通过kobject_uevent的方式通知的应用层，就是往一个socket
+    // 广播一个消息，只需要在应用层打开socket监听NETLINK_KOBJECT_UEVENT组的消息，就可以收
+    // 到了,主要是创建了socket接口获得uevent的文件描述符，然后触发/sys/class/power_supply
+    // 目录及其子目录下的uevent，然后接受并创建设备节点，至此设备节点才算创建。
     coldboot(charger, "/sys/class/power_supply", "add");
 
     ret = res_create_surface("charger/battery_fail", &charger->surf_unknown);
@@ -1333,6 +1371,7 @@ int main(int argc, char **argv)
         charger->surf_unknown = NULL;
     }
 
+    // (4)、这里是显示charger log，res_create_surface是显示图片函数
     for (i = 0; i < charger->batt_anim->num_frames; i++) {
         struct frame *frame = &charger->batt_anim->frames[i];
 
@@ -1346,6 +1385,7 @@ int main(int argc, char **argv)
         }
     }
 
+    // 这里是显示电池损坏动画？？？
     for (i = 0; i < battery_overtemp_animation.num_frames; i++) {
         struct frame *frame = &battery_overtemp_animation.frames[i];
 
@@ -1358,7 +1398,7 @@ int main(int argc, char **argv)
             break;
         }
     }
-
+    // 更新按键状态
     ev_sync_key_state(set_key_callback, charger);
 
 #ifndef CHARGER_DISABLE_INIT_BLANK
@@ -1374,7 +1414,7 @@ int main(int argc, char **argv)
 
     init_shutdown_alarm();
 
-    event_loop(charger);
+    event_loop(charger);    // (5)、event_loop循环，电池状态，检测按键是否按下
 
     return 0;
 }
