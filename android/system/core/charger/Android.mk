f3c9d7629e371858020dee63ae9425c34fca572e
labrick <yananback@gmail.com>
2016-10-23 17:48:40 +0800

    完善android/system/core/charger的Android.mk
    1. Android.mk注释必须另起一行，不能在代码行后直接加#注释
    2. 一旦修改了Android.mk，好像必须重新走编译android的整个流程才能通过，还不太明白

--- android/system/core/charger/Android.mk
+++ android/system/core/charger/Android.mk
@@ -2,9 +2,12 @@
 
 ifneq ($(BUILD_TINY_ANDROID),true)
 
+# 给出当前文件的路径
 LOCAL_PATH := $(call my-dir)
+# 这是一个脚本，用于清除之前的变量，避免影响
 include $(CLEAR_VARS)
 
+# 要编译的源代码列表
 LOCAL_SRC_FILES := \
 	charger.c \
 	lights.c
@@ -17,23 +20,34 @@ ifeq ($(strip $(BOARD_CHARGER_ENABLE_SUSPEND)),true)
 LOCAL_CFLAGS += -DCHARGER_ENABLE_SUSPEND
 endif
 
+# 模块名称，必须唯一，而且不能包含空格
 LOCAL_MODULE := charger
 LOCAL_MODULE_TAGS := optional
 LOCAL_FORCE_STATIC_EXECUTABLE := true
+# 编译结果的安装路径为根文件系统
+# TARGET_ROOT_OUT:根文件系统
+# TARGET_OUT:system文件系统
+# TARGET_OUT_DATA:data文件系统
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 LOCAL_UNSTRIPPED_PATH := $(TARGET_ROOT_OUT_UNSTRIPPED)
+# 上面两条命令的区别还不是很清楚
 
+# 头文件的搜索路径，默认LOCAL_PATH
 LOCAL_C_INCLUDES := bootable/recovery
 
+# 编译时搜需要使用哪些动态库？以便在编译时进行链接
 LOCAL_STATIC_LIBRARIES := libminui libpixelflinger_static libpng
 ifeq ($(strip $(BOARD_CHARGER_ENABLE_SUSPEND)),true)
 LOCAL_STATIC_LIBRARIES += libsuspend
 endif
 LOCAL_STATIC_LIBRARIES += libz libstdc++ libcutils liblog libm libc
 
+# 最终得到可执行文件
 include $(BUILD_EXECUTABLE)
 
+# 这里好像开始编译了另外一个模块，应该是图片资源
 define _add-charger-image
+# 两个$$啥意思？
 include $$(CLEAR_VARS)
 LOCAL_MODULE := system_core_charger_$(notdir $(1))
 LOCAL_MODULE_STEM := $(notdir $(1))
@@ -42,6 +56,8 @@ LOCAL_SRC_FILES := $1
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $$(TARGET_ROOT_OUT)/res/images/charger
+# 把他编译成一个独立的模块，提前编译成第三方库方便使用
+# 该库将被拷贝到$PROJECT/obj/local和$PROJECT/libs/<abi>(stripped)
 include $$(BUILD_PREBUILT)
 endef
 
